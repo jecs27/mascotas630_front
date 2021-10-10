3b0c7ee6a82c4e9f5b690da2ff13961d516cc89f
jecs <jesus.calderon2708@gmail.com>
2021-10-09 22:02:01 -0600

    proyecto front mascotas

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="mx-auto" max-width="344">
+    
+    <v-card-title>Coco </v-card-title>
+    <v-card-subtitle> Labrador </v-card-subtitle>
+    
+    <v-card-actions>
+      <v-btn icon @click="show = !show" >
+        <v-icon>{{ show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="green lighten-2" text >Editar</v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+        <img src="data:image/jpeg;base64," width="126px" >
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+      show: false,
+    }),
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-container grid-list-sm >
+  <v-layout row wrap>
+  <v-flex xs12 sm6 md4 >
+    <v-card class="mx-auto" max-width="344">
+      <v-card-title>Coco </v-card-title>
+      <v-card-subtitle> Labrador </v-card-subtitle>
+      <v-card-actions>
+        <v-btn icon @click="show = !show" >
+          <v-icon>{{ show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+         
+        <v-btn color="green lighten-2" text  @click="mostrarModal()">Editar</v-btn>
+      </v-card-actions>
+
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+          <img src="data:image/jpeg;base64," width="126px" >
+        </div>
+      </v-expand-transition>
+    </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
+
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<script>
+
+
+  export default {
+    data: () => ({
+      show: false,
+    }),
+    components:{
+      
+    },
+    methods:{
+      mostrarModal(){
+        console.log("entra");
+        this.dialog = true
+      }
+    }
   }
-}
 </script>
